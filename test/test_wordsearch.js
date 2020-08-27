5311d28f96d15b9312fc2b4f6626ba5af51a43dd
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-27 01:32:58 +0000

    Completed pair programming assignment with David R & David J

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,7 +1,7 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
@@ -31,8 +31,30 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
+
+  it("should return true if vertical word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'R', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'E', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'E', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'D', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'O', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'M', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'S', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], "FREEDOMS");
+
+    assert.isTrue(result);
+  });
+
+  it("should return false if letters is an empty array", function() {
+    const result = wordSearch([], 'BOB');
+    assert.isFalse(result);
+  });
+
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+//Nicole, David J, David R
+const transpose = function(matrix) {
+  let result = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (result[j] === undefined) {
+        result[j] = [];
+      }
+      result[j][i] = matrix[i][j];
     }
-}
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+module.exports = wordSearch;
